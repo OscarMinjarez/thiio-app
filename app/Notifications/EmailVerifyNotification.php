a9c53fa87b5dd09eeb052d0f0b4138c6d08c7c25
OscarMinjarez <minjarez.dev@gmail.com>
2024-07-08 21:49:29 -0700

    EmailVerification added

---
+++ app/Notifications/EmailVerifyNotification.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class EmailVerifyNotification extends Notification
+{
+    use Queueable;
+
+    protected $url;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->subject("Verify email address")
+                    ->subject("Dear $notifiable->name")
+                    ->line('Thank you for join us')
+                    ->action('Notification Action', $this->url)
+                    ->line('If you did not create an account, do nothing please');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
