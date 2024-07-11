f7ca94b7e03c48303f490b0dbad8746fea32ac88
OscarMinjarez <minjarez.dev@gmail.com>
2024-07-11 12:44:38 -0700

    Fix de response for get users with id's

--- app/Customs/Services/UsersService.php
+++ app/Customs/Services/UsersService.php
@@ -7,12 +7,12 @@ use App\Models\User;
 class UsersService {
 
     public function get_users() {
-        $users = User::select("name", "email", "created_at", "email_verified_at")
+        $users = User::select("id", "name", "email", "created_at", "email_verified_at")
                         ->where("email", "!=", auth()->user()->email)
                         ->get();
         return response()->json([
             "status" => "success",
             "users" => $users
-        ]);
+        ], 200);
     }
 }
